30b1d0813fb6550d78ef3c7962f518eaaf5e1eb0
ShaiberAlon <alon.shaiber@gmail.com>
2017-01-30 18:42:45 -0600

    cleanup

--- MACg/get_taxon_specific_genes_from_matrix.py
+++ MACg/get_taxon_specific_genes_from_matrix.py
@@ -1,59 +1,8 @@
 # -*- coding: utf-8
 # pylint: disable=line-too-long
 import csv
-import random
 import numpy as np
 
-def cartesian(arrays, out=None):
-    """
-    copied from:
-    Generate a cartesian product of input arrays.
-
-    Parameters
-    ----------
-    arrays : list of array-like
-        1-D arrays to form the cartesian product of.
-    out : ndarray
-        Array to place the cartesian product in.
-
-    Returns
-    -------
-    out : ndarray
-        2-D array of shape (M, len(arrays)) containing cartesian products
-        formed of input arrays.
-
-    Examples
-    --------
-    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
-    array([[1, 4, 6],
-           [1, 4, 7],
-           [1, 5, 6],
-           [1, 5, 7],
-           [2, 4, 6],
-           [2, 4, 7],
-           [2, 5, 6],
-           [2, 5, 7],
-           [3, 4, 6],
-           [3, 4, 7],
-           [3, 5, 6],
-           [3, 5, 7]])
-
-    """
-
-    arrays = [np.asarray(x) for x in arrays]
-    dtype = arrays[0].dtype
-
-    n = np.prod([x.size for x in arrays])
-    if out is None:
-        out = np.zeros([n, len(arrays)], dtype=dtype)
-
-    m = n / arrays[0].size
-    out[:,0] = np.repeat(arrays[0], m)
-    if arrays[1:]:
-        cartesian(arrays[1:], out=out[0:m,1:])
-        for j in range(1, arrays[0].size):
-            out[j*m:(j+1)*m,1:] = out[0:m,1:]
-    return out
 
 def get_data_from_txt_file(file_name):
     data_list = list(csv.reader(open(file_name), delimiter='\t'))
@@ -214,7 +163,6 @@ def get_accessory_genes(data, taxon_specific_genes, positive_samples, a=3):
     Ngenes = len(data)
     Ns = len(data[0])
     # indices of the sub-matrix containing all Taxon-specific genes only in positive samples:
-    # indices = cartesian((taxon_specific_genes, positive_samples))
     # calculating the mean of the taxon specific genes in each positive sample
     gene_detection = np.zeros((Ngenes, Ns), dtype=bool)
     for sample in positive_samples:
